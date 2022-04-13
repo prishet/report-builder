5d339ca8426ad14d4ffc551a49e4e1a5fa93eb84
prishet <101782888+prishet@users.noreply.github.com>
2022-04-13 13:18:25 +0530

    Add files via upload

---
+++ SafteyMangementApplication.java
@@ -0,0 +1,13 @@
+package com.example.saftey.mangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafteyMangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafteyMangementApplication.class, args);
+	}
+
+}
